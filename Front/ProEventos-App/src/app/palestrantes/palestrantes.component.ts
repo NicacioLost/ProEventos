de179288abf9ff657b46c703e1bf878ca423fa40
Anderson Nicacio <nicacio.tecnologia@gmail.com>
2021-04-24 20:03:51 -0300

    Seção 4: Angular

---
+++ Front/ProEventos-App/src/app/palestrantes/palestrantes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-palestrantes',
+  templateUrl: './palestrantes.component.html',
+  styleUrls: ['./palestrantes.component.scss']
+})
+export class PalestrantesComponent implements OnInit {
+
+  public palestrante: any = {
+    Tema: 'Angular',
+    Local: 'Belo Horizonte'
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
