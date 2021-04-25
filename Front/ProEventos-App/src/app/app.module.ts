46c73a3f87c610aaff567e5b319d450cf86cbf12
Anderson Nicacio <nicacio.tecnologia@gmail.com>
2021-04-25 13:31:58 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -2,23 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import {enableProdMode} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+enableProdMode();
+
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+    widthImg = 100;
+    marginImg = 2;
+    exibirImg = true;
+    // tslint:disable-next-line: variable-name
+    private _filtroLista = '';
+
+    // tslint:disable-next-line: typedef
+    public get filtroLista(): string {
+      return this._filtroLista;
+    }
+
+    public set filtroLista(value: string){
+       this._filtroLista = value;
+       this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+    }
+
+    filtrarEventos(filtrarPor: string): any {
+      filtrarPor = filtrarPor.toLocaleLowerCase();
+      return this.eventos.filter(
+        (evento: { tema: string; local: string; }) =>
+         evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+         evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
 
     constructor(private http: HttpClient) { }
 
@@ -16,28 +42,19 @@ export class EventosComponent implements OnInit {
       this.getEventos();
 
     }
+    // tslint:disable-next-line: typedef
+    alterarImagem(){
+      this.exibirImg = !this.exibirImg;
+    }
   public getEventos(): void {
+    // tslint:disable-next-line: deprecation
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      Response => this.eventos = Response,
+      response => {
+                    this.eventos = response;
+                    this.eventosFiltrados = this.eventos;
+                  },
+
       error => console.log(error)
     );
-
-
-    this.eventos =
-  [
-    {
-      Tema: 'Angular 11',
-      Local: 'Belo Horizonte'
-    },
-    {
-      Tema: '.NET 5',
-      Local: 'São Paulo'
-    },
-    {
-      Tema: 'Angular + .NET 5',
-      Local: 'Rio de Janeiro'
-    },
-  ];
   }
-
 }
